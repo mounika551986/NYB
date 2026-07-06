6a38e2194a594a86f0098451f9de732e02d59884
mounika <mounika.katirevula@nybinfotech.com>
2026-07-06 18:33:52 +0530

    initial commit

---
+++ React_06-07-2026/react-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Components from "./components/Components";
+function App(){
+
+
+return(
+  <BrowserRouter>
+
+  <Route path="/components" element={<Components/>}/>
+  
+    </BrowserRouter>
+
+)
+}
+export default App;
---
+++ student-card/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Student Card</h1>
+
+      <p><strong>Name:</strong> sravs</p>
+
+      <p><strong>Course:</strong> React JS</p>
+
+      <p><strong>College:</strong> ABC College</p>
+
+      <p><strong>Year:</strong> 3rd Year</p>
+
+      <button>Contact</button>
+    </div>
+  );
+}
+
+export default App;
